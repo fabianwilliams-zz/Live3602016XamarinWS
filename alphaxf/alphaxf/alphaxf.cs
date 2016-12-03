95968aa5c30a62690b119d6e49cc482853d1df26
Fabian Williams <fabian@adotob.com>
2016-12-03 17:14:06 -0500

    Adding generica Xam.Forms App

---
+++ alphaxf/alphaxf/alphaxf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace alphaxf
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "Fabian Alpha Demo",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome: Live360 App Trends XamWS!"
+						}
+					}
+				}
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
